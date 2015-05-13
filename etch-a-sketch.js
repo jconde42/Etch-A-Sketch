de580ebb8605d78bdc3dd85f164cb6d897a46937
Janner Conde <condejanner@gmail.com>
2015-05-13 11:28:37 -0600

    resize and grid toggle done.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,7 @@
+var border = false;
+
 $(document).ready( function() {
-    for( var i = 0; i < 256; i++) {
+    for( var i = 0; i < 625; i++) {
         $("#container").append("<div class='square'></div>");
     }
 
@@ -11,4 +13,36 @@ $(document).ready( function() {
         $(".black").removeClass("black");
     });
 
+    $(document.getElementById("grid")).click( function() {
+
+        if ( !(border) ) {
+            $(".square").css("border", "1px solid black");
+        }
+
+        if ( border ) {
+            $(".square").css("border", "");
+        }
+
+        border = !border;
+    })
+
+    $(document.getElementById("resize")).click( function() {
+        var rows = prompt("How many rows would you like? (1 - 25)");
+        var cols = prompt("How many columns would you like? (1 - 25)");
+
+        var newHeight = Math.floor(560/rows);
+        var newWidth = Math.floor(560/cols);
+
+        if( border) {
+           $(".square").css("border", "");
+       }
+
+       $(".square").width(newWidth + "px");
+       $(".square").height(newHeight + "px");
+
+       if( border) {
+           $(".square").css("border", "1px solid black");
+       }
+
+   })
 })
